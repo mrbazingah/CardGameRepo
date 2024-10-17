76edff92d0c946e2d0b8870cfce3da3457ce7c34
mrbazingah <143615546+mrbazingah@users.noreply.github.com>
2024-10-17 16:14:21 +0200

    TDOD stuff

--- Card Game/Assets/Scripts/TODO.cs
+++ Card Game/Assets/Scripts/TODO.cs
@@ -3,7 +3,8 @@ using UnityEngine;
 public class TODO : MonoBehaviour
 {
     //Sort cards by value
-    //Skip turn button if you don't want to play all your cards
     //Switch overside cards
     //See Ai's overside and underside cards
+    //Add animations with lerp and stuff
+    //Add sound effects
 }
