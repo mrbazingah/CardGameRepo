ff61a0055b06efcd33e8723a5ef8df6ce1a9534a
mrbazingah <143615546+mrbazingah@users.noreply.github.com>
2024-11-17 22:47:13 +0100

    cards are gay

--- Card Game/Assets/Scripts/Players/PlayerHand.cs
+++ Card Game/Assets/Scripts/Players/PlayerHand.cs
@@ -10,7 +10,7 @@ public class PlayerHand : MonoBehaviour
     [SerializeField] List<GameObject> underSideCards, overSideCards;
     [Header("Transform")]
     [SerializeField] Transform handTransform;
-    [SerializeField] float baseCardSpacing = 150f, verticalSpacing = 50f, maxHandWidth = 1000f, popUpHeight = 50f;
+    [SerializeField] float baseCardSpacing = 150f, maxHandWidth = 1000f, popUpHeight = 50f;
     [Space]
     [SerializeField] Transform underSideTransform, overSideTransform;
     [SerializeField] float sideBaseCardSpacing = 150f, sideVerticalSpacing = 50f, sideMaxHandWidth = 1000f, overSideOffset;
@@ -197,15 +197,15 @@ public class PlayerHand : MonoBehaviour
 
     public void SortCards()
     {
-        ArrangeCards(handCards, handTransform, baseCardSpacing, verticalSpacing, maxHandWidth);
-        ArrangeCards(overSideCards, overSideTransform, sideBaseCardSpacing, sideVerticalSpacing, sideMaxHandWidth, overSideOffset);
-        ArrangeCards(underSideCards, underSideTransform, sideBaseCardSpacing, sideVerticalSpacing, sideMaxHandWidth);
+        ArrangeCards(handCards, handTransform, baseCardSpacing, maxHandWidth);
+        ArrangeCards(overSideCards, overSideTransform, sideBaseCardSpacing, sideMaxHandWidth, overSideOffset);
+        ArrangeCards(underSideCards, underSideTransform, sideBaseCardSpacing, sideMaxHandWidth);
 
         ApplyHoverEffect(overSideCards, overSideTransform, sideBaseCardSpacing, sideVerticalSpacing, sideMaxHandWidth, overSideOffset);
         ApplyHoverEffect(underSideCards, underSideTransform, sideBaseCardSpacing, sideVerticalSpacing, sideMaxHandWidth);
     }
 
-    void ArrangeCards(List<GameObject> cards, Transform parent, float spacing, float verticalSpace, float maxWidth, float offset = 0)
+    void ArrangeCards(List<GameObject> cards, Transform parent, float spacing, float maxWidth, float offset = 0)
     {
         if (cards.Count == 0) return;
 
@@ -232,14 +232,8 @@ public class PlayerHand : MonoBehaviour
             if (cards.Count > 1)
             {
                 float horizontalOffset = cardSpacing * (i - (cards.Count - 1) / 2f);
-                float normalizedPosition = 2f * i / (cards.Count - 1) - 1f;
-                float verticalOffset = verticalSpace * (1 - normalizedPosition * normalizedPosition);
-                Vector3 cardPosition = new Vector3(horizontalOffset + offset, verticalOffset + offset, 0);
-
-                Vector2 popUpPosition = new Vector3(cards[i].transform.position.x, popUpHeight);
-                cards[i].transform.localPosition = cards[i] == hoveredCard ? 
-                    Vector2.Lerp(cards[i].transform.localPosition, popUpPosition, lerpSpeed) :
-                    Vector2.Lerp(cards[i].transform.localPosition, cardPosition, lerpSpeed);
+                Vector2 cardPosition = cards[i] == hoveredCard ? new Vector2(horizontalOffset + offset, offset + popUpHeight) : new Vector2(horizontalOffset + offset, offset);
+                cards[i].transform.localPosition = Vector2.Lerp(cards[i].transform.localPosition, cardPosition, lerpSpeed);
             }
             else
             {
