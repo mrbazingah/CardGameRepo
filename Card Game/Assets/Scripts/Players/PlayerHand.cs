e65acf8a075c9b354adf042bcf6f2263ac37679b
mrbazingah <143615546+mrbazingah@users.noreply.github.com>
2026-04-26 13:17:34 +0200

    0006: Fix pickup side cards with pile

--- Card Game/Assets/Scripts/Players/PlayerHand.cs
+++ Card Game/Assets/Scripts/Players/PlayerHand.cs
@@ -422,6 +422,8 @@ public class PlayerHand : MonoBehaviour
 
                 if (!hasCardToPlay)
                 {
+                    pile.AddCardsToPile(cardInHand);
+                    RemoveCardFromList(cardInHand);
                     PickUpPile(cardInHand);
                 }
             }
